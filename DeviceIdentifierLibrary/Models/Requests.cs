2d12d2aed0334d58eae7acce6a3dec2ae1d05064
Siti Syaziyah Binti Samiri <sitisyaziyah@senergy.com.my>
2026-06-04 11:13:33 +0800

    removal AssemblyInfo.cs old version NET4.7.2

---
+++ DeviceIdentifierLibrary/Models/Requests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeviceIdentifierLibrary.Models
+{
+	public class DevicePredictRequest
+	{
+		public string project_code { get; set; }
+		public string customer_code { get; set; }
+		public List<string> data_ids { get; set; }
+	}
+
+	public class ManualAssignment
+	{
+		public string data_id { get; set; }
+		public string equipment { get; set; }
+	}
+
+	public class UserManualAssignRequest
+	{
+		public string action { get; set; } = "user_manual_assign";
+		public string project_code { get; set; }
+		public string customer { get; set; }
+		public List<ManualAssignment> assignments { get; set; }
+	}
+
+	public class PipelinePredictRequest
+	{
+		public List<PipelineRecord> records { get; set; }
+	}
+
+	public class PipelineRecord
+	{
+		public string device_id { get; set; }
+		public string customer { get; set; }
+		public string project { get; set; }
+	}
+}
---
+++ DeviceIdentifierLibrary/Services/PythonClient.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace DeviceIdentifierLibrary.Services
+{
+	public class PythonClient
+	{
+		private readonly string _pythonExe;
+
+		public PythonClient(string pythonExe)
+		{
+			_pythonExe = pythonExe;
+		}
+
+		public async Task<string> RunAsync(string scriptPath, object request)
+		{
+			string jsonInput = JsonSerializer.Serialize(request);
+
+			var psi = new ProcessStartInfo
+			{
+				FileName = _pythonExe,
+				Arguments = $"-u \"{scriptPath}\"",
+				RedirectStandardInput = true,
+				RedirectStandardOutput = true,
+				RedirectStandardError = true,
+				StandardOutputEncoding = Encoding.UTF8,
+				StandardErrorEncoding = Encoding.UTF8,
+				UseShellExecute = false,
+				CreateNoWindow = true
+			};
+
+			using var process = new Process { StartInfo = psi };
+			process.Start();
+
+			var outputTask = process.StandardOutput.ReadToEndAsync();
+			var errorTask = process.StandardError.ReadToEndAsync();
+
+			try
+			{
+				await process.StandardInput.WriteAsync(jsonInput);
+				process.StandardInput.Close();
+			}
+			catch (IOException) { }
+
+			string output = await outputTask;
+			string error = await errorTask;
+			await process.WaitForExitAsync();
+
+			if (process.ExitCode != 0)
+				throw new Exception($"Python script error:\n{error}");
+
+			return output.Trim();
+		}
+	}
+}
