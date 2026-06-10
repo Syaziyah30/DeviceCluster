f363344d38db5aa2b1dc89e1df844db9f3c2dc46
Siti Syaziyah Binti Samiri <sitisyaziyah@senergy.com.my>
2026-06-10 15:58:25 +0800

    create separate class for reuslt.cs

---
+++ DeviceIdentifierLibrary/ModelResult/DeviceTypeResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeviceIdentifierLibrary.ModelResult
+{
+	public class DeviceTypeResult
+	{
+		public string customer { get; set; }
+		public string data_id { get; set; }
+		public string manual_check { get; set; }
+		public string data_type { get; set; }
+		public double? confidence { get; set; }
+		public string reason { get; set; }
+	}
+}
---
+++ DeviceIdentifierLibrary/ModelResult/PipelineResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeviceIdentifierLibrary.ModelResult
+{
+	public class PipelineResult
+	{
+		public string DEVICE_ID { get; set; }
+		public string CUSTOMER { get; set; }
+		public string PROJECT { get; set; }
+		public string PREDICTED_SECTION { get; set; }
+		public double? SECTION_CONFIDENCE { get; set; }
+		public string PREDICTED_CLUSTER { get; set; }
+		public double? CLUSTER_CONFIDENCE { get; set; }
+		public string REJECTION_REASON { get; set; }
+		public string FORMAT_WARNING { get; set; }
+	}
+}
