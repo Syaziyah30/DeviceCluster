46c581de01f8795b3cf5a66b0ef6808a804c2e62
Siti Syaziyah Binti Samiri <sitisyaziyah@senergy.com.my>
2026-07-24 11:20:56 +0800

    PipelinePredictRequest.cs  - insert the setting predict cluster output csv

--- Model/ModelRequest/PipelinePredictRequest.cs
+++ Model/ModelRequest/PipelinePredictRequest.cs
@@ -5,8 +5,8 @@ using Model.ModelRequest;
 
 namespace Model.ModelRequest
 {
-	public class PipelinePredictRequest
-	{
-		public List<PipelineRecord> records { get; set; }
-	}
+public class PipelinePredictRequest
+{
+    public List<PipelineRecord> records { get; set; }
+    public string? export_raw_csv_path { get; set; }   // ◄── ADDED
 }
